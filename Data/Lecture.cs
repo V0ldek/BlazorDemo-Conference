b641c3872eec8673b4ff13f7e2b42c737a8b36d5
Mateusz Gienieczko <mat@gienieczko.com>
2021-12-19 15:29:17 +0100

    My commit.

--- Data/Lecture.cs
+++ Data/Lecture.cs
@@ -6,6 +6,8 @@ public class Lecture
 
     public DateTime When { get; set; }
 
+    public Author? Speaker { get; set; }
+
     public Lecture(int lectureId, DateTime when) => 
         (Id, When) = (lectureId, when);
 }
