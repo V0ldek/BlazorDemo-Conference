77a2d508b3c818a8344cd263e96f43b7e5a6428a
Kapiszon2343 <harasimowiczk@wp.pl>
2022-01-02 00:41:53 +0100

    fixed tabs

--- Data/Lecture.cs
+++ Data/Lecture.cs
@@ -2,21 +2,21 @@ namespace Conference.Data;
 
 public class Lecture
 {
-  public int Id { get; init; }
+    public int Id { get; init; }
 
-  public DateTime When { get; set; }
+    public DateTime When { get; set; }
 
-  public Author Speaker { get; set; }
+    public Author Speaker { get; set; }
 
-  public Author Chair { get; set; }
+    public Author Chair { get; set; }
 
-  public Paper Paper_used { get; set; }
+    public Paper Paper_used { get; set; }
 
-  public Lecture(int lectureId,
-                 DateTime when,
-                 Author speaker,
-                 Paper paper,
-                 Author session) =>
-      (Id, When, Speaker, Chair, Paper_used) =
-          (lectureId, when, speaker, session, paper);
+    public Lecture(int lectureId,
+                   DateTime when,
+                   Author speaker,
+                   Paper paper,
+                   Author session) =>
+        (Id, When, Speaker, Chair, Paper_used) =
+            (lectureId, when, speaker, session, paper);
 }
