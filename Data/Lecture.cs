5c2b7ede45f818af4cdd4100174bdca7a834859b
Kapiszon2343 <46422065+Kapiszon2343@users.noreply.github.com>
2022-01-01 23:29:21 +0100

    All fields in Lecture filled

--- Data/Lecture.cs
+++ Data/Lecture.cs
@@ -1,11 +1,22 @@
 namespace Conference.Data;
 
-public class Lecture 
+public class Lecture
 {
-    public int Id { get; init; }
+  public int Id { get; init; }
 
-    public DateTime When { get; set; }
+  public DateTime When { get; set; }
 
-    public Lecture(int lectureId, DateTime when) => 
-        (Id, When) = (lectureId, when);
+  public Author Speaker { get; set; }
+
+  public Author Chair { get; set; }
+
+  public Paper Paper_used { get; set; }
+
+  public Lecture(int lectureId,
+                 DateTime when,
+                 Author speaker,
+                 Paper paper,
+                 Author session) =>
+      (Id, When, Speaker, Chair, Paper_used) =
+          (lectureId, when, speaker, session, paper);
 }
