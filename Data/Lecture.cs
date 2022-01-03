cc5145e61ebf20f79981e67779a2a9d19bd3fe84
Kapiszon2343 <harasimowiczk@wp.pl>
2022-01-03 19:42:43 +0100

    elements of Lectures can be set later

--- Data/Lecture.cs
+++ Data/Lecture.cs
@@ -6,17 +6,14 @@ public class Lecture
 
     public DateTime When { get; set; }
 
-    public Author Speaker { get; set; }
+    public Author? Speaker { get; set; }
 
-    public Session Session { get; set; }
+    public Session? Session { get; set; }
 
-    public Paper Paper_used { get; set; }
+    public Paper? Paper_used { get; set; }
 
     public Lecture(int lectureId,
-                   DateTime when,
-                   Author speaker,
-                   Paper paper,
-                   Session session) =>
-        (Id, When, Speaker, Session, Paper_used) =
-            (lectureId, when, speaker, session, paper);
+                   DateTime when) =>
+        (Id, When) =
+            (lectureId, when);
 }
