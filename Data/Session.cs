e8847aec73d9d935e0d72ca22ab6fa26de4c8990
Kapiszon2343 <harasimowiczk@wp.pl>
2022-01-03 19:53:00 +0100

    set lectures

--- Data/Session.cs
+++ Data/Session.cs
@@ -1,14 +1,14 @@
 namespace Conference.Data;
 
-public class Session 
+public class Session
 {
     public int Id { get; init; }
 
     public DateTime When { get; init; }
 
     public Author? Chair { get; set; }
-    
-    public List<Lecture> Lectures { get; init; } = new List<Lecture>();
+
+    public List<Lecture> Lectures { get; set; } = new List<Lecture>();
 
     public Session(int sessionId, DateTime when) =>
         (Id, When) = (sessionId, when);
