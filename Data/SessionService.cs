262cefc2ad4cc238c3e9517b135f82c4488df82f
Kapiszon2343 <harasimowiczk@wp.pl>
2022-01-04 13:01:44 +0100

    Wypełnianie session.Lectures

--- Data/SessionService.cs
+++ Data/SessionService.cs
@@ -10,8 +10,9 @@ public sealed class SessionService
     public SessionService(IDbConnection dbConnection) =>
         _dbConnection = dbConnection;
 
-    public IReadOnlyList<Session> GetSessions() =>
-        _dbConnection.Query<Session, Author, Session>(
+    public IReadOnlyList<Session> GetSessions()
+    {
+        List<Session> lst = _dbConnection.Query<Session, Author, Session>(
             @"SELECT s.id AS sessionId, s.when, a.id AS authorId, a.name, a.surname 
                 FROM session s
                 JOIN author a
@@ -23,6 +24,60 @@ public sealed class SessionService
             },
             splitOn: "authorId")
             .ToList();
+
+        var paperDictionary = new Dictionary<int, Paper>();
+
+        var papers = _dbConnection.Query<Paper, Author, Paper>(
+            @"SELECT p.id AS paperId, p.name, p.classification, a.id AS authorId, a.name, a.surname 
+                FROM paper p
+                LEFT JOIN paper_author pa
+                    ON p.id = pa.paper_id
+                LEFT JOIN author a
+                    ON pa.author_id = a.id",
+            (p, a) =>
+            {
+                Paper? paper;
+
+                if (!paperDictionary.TryGetValue(p.Id, out paper))
+                {
+                    paper = p;
+                    paperDictionary.Add(paper.Id, paper);
+                }
+
+                if (a is not null)
+                {
+                    paper.Authors.Add(a);
+                }
+
+                return paper;
+            },
+            splitOn: "authorId");
+
+        foreach (Session session in lst)
+        {
+            session.Lectures = _dbConnection.Query<Lecture, Author, Paper, Lecture>(
+                @"SELECT l.id AS lectureId, l.when, s.id AS authorId, s.name, s.surname, p.id AS paperId, p.name, p.classification
+                    FROM lecture l
+                    JOIN author s
+                        ON l.speaker_id = s.id
+                    JOIN paper p
+                        ON l.paper_id = p.id
+                    WHERE l.session_id = " + session.Id.ToString(),
+                    (l, sp, p) =>
+                    {
+                        l.Speaker = sp;
+                        Paper? paper;
+                        paperDictionary.TryGetValue(p.Id, out paper);
+                        l.Paper_used = paper;
+                        l.Session = session;
+                        return l;
+                    },
+            splitOn: "authorId, paperId"
+            ).ToList();
+        }
+
+        return lst;
+    }
 
     public void CreateSession(CreateSession model)
     {
