37b729de56ac890a3b94a3900580156f85da5a14
karqz <karqzkolu@gmail.com>
2022-01-07 17:40:28 +0100

    Mikrozadanie.

--- Data/Lecture.cs
+++ Data/Lecture.cs
@@ -6,6 +6,10 @@ public class Lecture
 
     public DateTime When { get; set; }
 
+    public Paper? Paper { get; set; }
+
+    public Author? Speaker { get; set; }
+
     public Lecture(int lectureId, DateTime when) => 
         (Id, When) = (lectureId, when);
 }
--- Data/SessionService.cs
+++ Data/SessionService.cs
@@ -10,19 +10,59 @@ public sealed class SessionService
     public SessionService(IDbConnection dbConnection) =>
         _dbConnection = dbConnection;
 
-    public IReadOnlyList<Session> GetSessions() =>
-        _dbConnection.Query<Session, Author, Session>(
-            @"SELECT s.id AS sessionId, s.when, a.id AS authorId, a.name, a.surname 
-                FROM session s
-                JOIN author a
-                    ON s.chair_id = a.id",
-            (s, a) =>
+    public IReadOnlyList<Session> GetSessions() {
+        var sessionDictionary = new Dictionary<int, Session>();
+
+        var sessions = _dbConnection.Query<Session, Author, Lecture, Author, Paper, Author, Session>(
+            @"SELECT s.id AS sessionId, s.""when"", 
+                     ch.id AS authorId, ch.name, ch.surname, 
+                     l.id AS lectureId, l.""when"",
+                     sp.id AS authorId, sp.name, sp.surname,
+                     p.id AS paperId, p.name, p.classification,
+                     a.id AS authorId, a.name, a.surname
+              FROM ""session"" s
+              JOIN author ch
+                ON s.chair_id = ch.id
+              LEFT JOIN lecture l
+                ON l.session_id = s.id
+              LEFT JOIN paper p
+                ON p.id = l.paper_id
+              LEFT JOIN author sp
+                ON sp.id = l.speaker_id
+              LEFT JOIN paper_author pa
+                ON pa.paper_id = p.id
+              LEFT JOIN author a
+                ON a.id = pa.author_id",
+            (s, ch, l, sp, p, a) =>
             {
-                s.Chair = a;
-                return s;
+                Session? session;
+
+                if (!sessionDictionary.TryGetValue(s.Id, out session)) {
+                    session = s;
+                    sessionDictionary.Add(session.Id, session);
+                }
+
+                if (!session.Lectures.Exists(a => a.Id == l.Id)) {
+                    p.Authors.Add(a);
+                    l.Paper = p;
+                    l.Speaker = sp;
+                    session.Lectures.Add(l);
+                }
+                else {
+                    Lecture? lecture = session.Lectures.Find(x => x.Id == l.Id);
+                    if (lecture is not null && lecture.Paper is not null) {
+                        lecture.Paper.Authors.Add(a);
+                    }
+                }
+                session.Chair = ch;
+
+                return session;
             },
-            splitOn: "authorId")
-            .ToList();
+            splitOn:"authorId,lectureId,authorId,paperId,authorId"
+        );
+
+        return sessions.Distinct().ToList();
+    }
 
     public void CreateSession(CreateSession model)
     {
