9f66bc80dd90eb3aca30171b249ae8182619b158
Mateusz Gienieczko <mat@gienieczko.com>
2022-01-11 23:41:03 +0100

    Model solution.

--- Data/Paper.cs
+++ Data/Paper.cs
@@ -1,6 +1,6 @@
 namespace Conference.Data;
 
-public class Paper 
+public class Paper
 {
     public int Id { get; init; }
 
@@ -8,7 +8,9 @@ public class Paper
 
     public string Classification { get; init; }
 
-    public List<Author> Authors { get; init; } = new List<Author>();
+    public string AuthorNames => string.Join(", ", Authors.Select(a => a.DisplayName));
+
+    public HashSet<Author> Authors { get; init; } = new HashSet<Author>();
 
     public Paper(int paperId, string name, string classification) =>
         (Id, Name, Classification) = (paperId, name, classification);
